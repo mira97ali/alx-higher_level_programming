0782bb7c4e6fde7ac2b41dae82e7f8c9910a2c0b
Mira Ali <benamara97amira@gmail.com>
2023-11-28 23:32:32 +0300

    add unnecessary format just because you asked for

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(90, 64, -1):
-    print(chr(i).lower() if i % 2 == 0 else chr(i), end="")
+    print("{}".format(chr(i).lower() if i % 2 == 0 else chr(i)), end="")
