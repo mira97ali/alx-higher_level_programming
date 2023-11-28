017ceb0cc48045696a902d387bd9d6b7e6fdfeff
Mira Ali <benamara97amira@gmail.com>
2023-11-28 22:15:37 +0300

    combinations fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(x + 1, 10):
-        print("{:02d}{:02d}".format(x, y), end=", " if x < 8 or y < 9 else "{:02d}{:02d}\n".format(x, y))
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        elif i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
