8815038dfe2829d7070e771c43d0ac68ae7ad3f7
Mira Ali <benamara97amira@gmail.com>
2023-12-03 20:22:17 +0300

    fix pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for vector in matrix:
         for idx, element in enumerate(vector):
-            print("{:d}".format(element), end=" " if idx < len(vector) - 1 else "")
+            print(
+                "{:d}".format(element),
+                end=" " if idx < len(vector) - 1 else ""
+            )
         print("{}".format("\n"), end="")
