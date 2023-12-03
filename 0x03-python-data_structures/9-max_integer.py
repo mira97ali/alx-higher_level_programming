d19a082559490d10ce61650a16ae674b5f524710
Mira Ali <benamara97amira@gmail.com>
2023-12-03 21:00:31 +0300

    maximum

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    result = 0
+    for i in my_list:
+        if i > result:
+            result = i
+    return result
