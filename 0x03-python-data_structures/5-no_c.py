215ba10efcf36e143261e4c2b59082d832cbc5a9
Mira Ali <benamara97amira@gmail.com>
2023-12-03 19:44:12 +0300

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for idx in range(0, len(my_string)):
-        if my_string[idx] == "c" or my_string[idx] == "C":
-            my_string[idx] = " "
-    return my_string
+    new_string = ""
+    for i in my_string:
+        if i == "c" or i == "C":
+            new_string = new_string + " "
+        else:
+            new_string = new_string + i
+    return new_string
