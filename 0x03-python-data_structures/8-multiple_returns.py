b2566f8eb1731166ebd7f19d9dc7aae5b0e11949
Mira Ali <benamara97amira@gmail.com>
2023-12-03 20:52:48 +0300

    length

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     if not sentence:
         return (0, None)
-    lenght = len(sentence)
+    length = len(sentence)
     first_char = sentence[0]
-    return (lenght, first_char)
+    return (length, first_char)
