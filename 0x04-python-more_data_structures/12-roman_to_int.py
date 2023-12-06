65bc7ab9f321b10d4c44e71a403996c591dbed97
Mira Ali <benamara97amira@gmail.com>
2023-12-06 23:46:35 +0300

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+ROMAN_NUMERALS = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+
+def roman_to_int(roman_string):
+    result = 0
+    prev_value = 0
+    for char in reversed(roman_string):
+        value = ROMAN_NUMERALS[char]
+
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+
+        prev_value = value
+
+    return result
