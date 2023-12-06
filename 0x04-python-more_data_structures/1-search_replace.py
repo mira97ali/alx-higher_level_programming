ddaf5af3e6072e81db342a0a37b4616a6fa496d8
Mira Ali <benamara97amira@gmail.com>
2023-12-06 21:00:43 +0300

    fix bug

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            new_list.append(my_list[i])
+    for key, value in enumerate(my_list):
+        if value == search:
+            new_list.append(replace)
+        else:
+            new_list.append(value)
     return new_list
