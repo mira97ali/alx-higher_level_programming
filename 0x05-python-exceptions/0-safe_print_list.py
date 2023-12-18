0eb9c5fe3e864b9bbe65a7ac9d11d3c2ceb442be
Mira Ali <benamara97amira@gmail.com>
2023-12-18 19:09:09 +0300

    remove empty list check

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,5 @@ def safe_print_list(my_list=[], x=0):
             number = number + 1
         except IndexError:
             break
-    if my_list != []:
-        print("\n", end="")
+    print("\n", end="")
     return number
