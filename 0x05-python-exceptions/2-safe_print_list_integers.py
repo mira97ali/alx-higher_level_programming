48733c245bf6fc0bf1070801acc2488b32bb8d65
Mira Ali <benamara97amira@gmail.com>
2023-12-18 19:37:23 +0300

    print int only without handle IOOR

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,13 +4,10 @@ def safe_print_list_integers(my_list=[], x=0):
     number = 0
     for i in range(x):
         try:
-            try:
-                result = "{:d}".format(my_list[i])
-            except (ValueError, TypeError):
-                continue
-            print(result, end="")
-            number = number + 1
-        except IndexError:
-            break
+            result = "{:d}".format(my_list[i])
+        except (ValueError, TypeError):
+            continue
+        print(result, end="")
+        number = number + 1
     print("\n", end="")
     return number
