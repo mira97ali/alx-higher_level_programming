71bf194644aa3ba4deda4b9c2a836bd6d016bca4
Mira Ali <benamara97amira@gmail.com>
2023-12-19 21:41:09 +0300

    size of square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Represents a square
+
+    Attributes:
+        size: Size of a given square.
+    """
+
+    def __init__(self, size):
+        self.__size = size
