4c87748b5474e4d9575f48662e83cdfc123c0418
Mira Ali <benamara97amira@gmail.com>
2023-12-19 22:10:48 +0300

    task five

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,6 +10,7 @@ class Square:
 
     Methods:
         area: Returning the current square area.
+        size: Set and get the square's size.
     """
 
     def __init__(self, size=0):
@@ -21,3 +22,13 @@ class Square:
 
     def area(self):
         return self.__size ** 2
+
+    def size(self):
+        return self.__size
+    
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
