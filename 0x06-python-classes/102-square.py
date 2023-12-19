82ef11d8e18f458417f72659d06706d65a0cd66f
Mira Ali <benamara97amira@gmail.com>
2023-12-19 23:36:57 +0300

    Compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Represents a square
+
+    Attributes:
+        size: Size of a given square.
+
+    Methods:
+        area: Returning the current square area.
+        size: Set and get the square's size.
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, (float, int)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
