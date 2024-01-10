34c2e279eaf24799f6d066f3a6acbe569da1c62d
Mira Ali <benamara97amira@gmail.com>
2024-01-10 23:33:07 +0300

    try fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,8 +5,15 @@
 def class_to_json(obj):
     """Class to JSON"""
     obj_dict = obj.__dict__.copy()
-    for key, _ in obj_dict.items():
+
+    keys_to_remove = []
+    for key, value in obj_dict.items():
         if key.startswith("_"):
             new_key = "{}{}".format(obj.__class__.__name__, key)
             obj_dict[new_key] = obj_dict.pop(key)
+            keys_to_remove.append(key)
+
+    for key in keys_to_remove:
+        obj_dict.pop(key, None)
+
     return obj_dict
