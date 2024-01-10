11ebdc3b2798095cde031410d9a1fa12f0616543
Mira Ali <benamara97amira@gmail.com>
2024-01-10 23:43:20 +0300

    fix pycodestyle

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,7 +8,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """Convert to JSON"""
         return self.__dict__
