7ac1c30dec61b187f0cde9100a0e01f4eea02bdb
Mira Ali <benamara97amira@gmail.com>
2024-01-11 00:12:41 +0300

    cleanup

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """Log parsing"""
-
+import sys
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
-
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
+    """Print accumulated metrics"""
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
-    import sys
-
+    
     size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
