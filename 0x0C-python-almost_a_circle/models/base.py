9375cdd6a0299d4b6807a78d5872b603ea8150cf
Mira Ali <benamara97amira@gmail.com>
2024-01-12 21:56:09 +0300

    creates base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Base class"""
+
+
+class Base:
+    """Creates the base class of shapes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
