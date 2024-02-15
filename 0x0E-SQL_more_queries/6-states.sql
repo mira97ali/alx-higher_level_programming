e5b9f9e29b090b91c70c7b09b0be8b00aa3ebea3
Mira Ali <benamara97amira@gmail.com>
2024-02-15 21:01:06 +0300

    States

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Create the database hbtn_0d_usa if it does not already exist
+-- Then select the database for subsequent operations
+-- Finally create the table states with specified requirements if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
