b35d6c0f68981c51ae5841cfa9ff13870d86f8ed
Mira Ali <benamara97amira@gmail.com>
2024-02-15 21:23:34 +0300

    rating shows

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,8 @@
--- This script lists all shows from hbtn_0d_tvshows_rate by their rating sum
--- Each record displays the show title and its total rating
+-- This script lists all shows from hbtn_0d_tvshows_rate by their rating
+-- Each record displays the show title and the sum of its ratings
 -- Results are sorted in descending order by the rating
-SELECT s.title, SUM(r.rating) AS rating_sum
-FROM tv_shows AS s
-JOIN tv_show_ratings AS r ON s.id = r.show_id
-GROUP BY s.id
-ORDER BY rating_sum DESC, s.title ASC;
+SELECT title, SUM(tv_show_ratings.rate) AS 'rating'
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
