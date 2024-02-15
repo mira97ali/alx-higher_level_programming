4f102b1687885320b44a6a817a60b2633e234ec5
Mira Ali <benamara97amira@gmail.com>
2024-02-15 21:13:49 +0300

    count shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,9 @@
--- List all genres from the hbtn_0d_tvshows database and count the number of shows linked to each genre
--- Then each record will display the genre and the number of shows linked to that genre
--- Then genres without any shows linked will not be displayed
--- Then results are sorted in descending order by the number of shows linked
--- Finally this query utilizes INNER JOIN to ensure only genres with linked shows are counted and GROUP BY to aggregate results by genre
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
+-- List all genres from the hbtn_0d_tvshows database and the number of shows linked to each
+-- Then only display genres with at least one show linked and sort the results by the number of linked shows in descending order
+-- Then use RIGHT JOIN to include all entries from tv_show_genres and matching entries from tv_genres
+-- Finally count the number of shows linked to each genre, grouping the results by genre
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres 
+RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
 ORDER BY number_of_shows DESC;
