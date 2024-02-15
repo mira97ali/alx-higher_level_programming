682e9ae7a53fde1c0dfed86d241f1e3e251eb606
Mira Ali <benamara97amira@gmail.com>
2024-02-15 20:26:51 +0300

    show grants only

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,8 +1,3 @@
--- Create the users if they do not exist
--- Then grant privileges to user_0d_1
--- Finally attempt to list privileges for user_0d_1 and user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- List privileges for user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
