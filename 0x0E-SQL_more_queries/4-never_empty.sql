102b070f1e57b1d45b7a160d8a426f53c9931758
Mira Ali <benamara97amira@gmail.com>
2024-02-15 20:57:27 +0300

    never empty

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Ensure the table id_not_null does not already exist before creation
+-- Create table with id INT defaulting to 1 and name as VARCHAR(256)
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
