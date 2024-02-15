a0558e10373ea6eb69db00610adef77e0de27bfd
Mira Ali <benamara97amira@gmail.com>
2024-02-15 21:07:36 +0300

    cities by state join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,9 @@
--- List all cities along with their corresponding state name using a subquery for state name matching
-SELECT c.id, c.name, (SELECT s.name FROM states AS s WHERE s.id = c.state_id) AS state_name
-FROM cities AS c
-ORDER BY c.id ASC;
+-- Select all cities and their corresponding state names from the database
+-- Then each record will display cities.id, cities.name, and states.name
+-- Then the results are sorted in ascending order by cities.id
+-- Then this is achieved by performing a LEFT JOIN between the cities and states tables
+-- Finally the JOIN condition is that the states.id matches cities.state_id
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+LEFT JOIN states ON states.id = cities.state_id 
+ORDER BY cities.id;
