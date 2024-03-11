0b875fec8af031c9c6828bdc43f8fe43ab43dc7f
Mira Ali <benamara97amira@gmail.com>
2024-03-12 00:55:20 +0300

    fix indentation

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,7 +7,7 @@ function factorial (n) {
   if (n === 0 || isNaN(n)) {
     return (1);
   }
-    return (n * factorial(n - 1));
+  return (n * factorial(n - 1));
 }
 
 console.log(factorial(Number(process.argv[2])));
