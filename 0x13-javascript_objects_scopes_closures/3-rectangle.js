5015cb40dc87ab26338d546d75f468808e96e83c
Mira Ali <benamara97amira@gmail.com>
2024-03-12 22:25:19 +0300

    fix indentation

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,20 +2,20 @@
 
 class Rectangle {
   constructor (w, h) {
-      if ((w > 0) && (h > 0)) {
-        this.width = w;
-        this.height = h;
-      }
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
   print () {
-      for (let i = 0; i < this.height; i++) {
-        let s = '';
-        for (let j = 0; j < this.width; j++) {
-            s += 'X';
-        }
-        console.log(s);
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
       }
+      console.log(s);
+    }
   }
 }
 
