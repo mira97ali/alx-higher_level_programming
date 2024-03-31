1d067b3b5b7ccab379d6cf6aa7b569d691ee332e
Mira Ali <benamara97amira@gmail.com>
2024-04-01 00:22:06 +0300

    What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Send Request"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
