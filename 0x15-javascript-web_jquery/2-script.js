2cf93e7a3d0342d9082fd02862226a60958a580f
Mira Ali <benamara97amira@gmail.com>
2024-05-02 22:34:58 +0100

    addall

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,4 @@
+$('DIV#red_header').on('click', function() {
+    $('HEADER').css('color', '#FF0000');
+  });
+  
---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,3 @@
+$.get('https://swapi.co/api/people/5/?format=json').done(function(data) {
+  $('DIV#character').text(data.name);
+});
